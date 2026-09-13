61b0bb9dd78968140c8dec9952660d28c8ec165d
Masud Ibn Musa <masudibnmusa10@gmail.com>
2026-09-13 22:55:48 +0600

    updating client/src/components/chat/MessageList.jsx

--- client/src/components/chat/MessageList.jsx
+++ client/src/components/chat/MessageList.jsx
@@ -1,4 +1,4 @@
-// components/chat/MessageList.jsx
+// client/src/components/chat/MessageList.jsx
 import { useRef } from 'react'
 import { useChatStore } from '../../store/chatStore'
 import { useScrollToBottom } from '../../hooks/useScrollToBottom'
@@ -6,8 +6,12 @@ import MessageBubble from './MessageBubble'
 import StreamingMessage from './StreamingMessage'
 import './MessageList.css'
 
+const EMPTY_MESSAGES = []
+
 function MessageList() {
-  const messages = useChatStore((state) => state.messages)
+  const messages = useChatStore(
+    (state) => state.conversations[state.activeConversationId]?.messages ?? EMPTY_MESSAGES
+  )
   const isStreaming = useChatStore((state) => state.isStreaming)
   const streamingText = useChatStore((state) => state.streamingText)
 
